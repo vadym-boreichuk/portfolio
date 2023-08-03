5bbc6f8e7b3d8827fdbe2a7f92a0b66d8a543c26
Vadym Boreichuk <109723184+vadym-boreichuk@users.noreply.github.com>
2023-08-03 20:35:53 +0200

    Update User.jsx

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -3,7 +3,7 @@ import { Container } from "./User.styled";
 export const User = () => {
   return (
     <Container>
-      <p>Photo</p>
+      <p>Update</p>
       <p>Name</p>
       <p>Surname</p>
     </Container>
